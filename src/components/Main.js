bc8878296ec394f6f1473faba36f189b7cc043a0
Ebuka <gideonebuka6@gmail.com>
2024-12-02 11:50:37 +0000

    4th commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,90 +11,42 @@ function Main() {
         <p>Beat NFT Sales</p>
         <p>Blockchain Music/Beat Aunctioning</p>
       </div>
-      <div className='mt-10'>
-        <h1>OUR POPULAR COLLECTIONS</h1>
+      <div className='mt-10 '>
+        <h1 className="underline font-bold text-lg">OUR POPULAR COLLECTIONS</h1>
+
         <div className=' mt-10 grid grid-cols-2 gap-5 '>
 
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src="image/1.jpeg" alt='' className='object-cover '/>
-            <audio>
-              <source src="audio/1.mp4"/>
+          <div className='rounded shadow-md grid p-4 bg-gray-600 relative'>
+            <img src="image/1.jpeg" alt='' className='w-full h-32 sm:h-48 object-cover'/>
+            <div className="flex justify-between font-semibold px-5 text-white py-2">
+              <p className="uppercase">Sam</p>
+              <p>Ghost Beat</p>
+            </div>
+            <audio controls className="">
+            <source src="audio/1.mpeg"/>
             </audio>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
             <div>
               <button className='bg-orange-600 rounded-lg text-white gap-10 p-2'>SOLD</button>
             </div>
+            <div className="absolute top-0 bg-white rounded-lg p-2 m-4 font-bold">
+              <span>$2000</span>
+            </div>
           </div>
 
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src='image/1.jpeg' alt='' className='w-full h-32 sm:h-48 object-cover'/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
+          <div className='rounded shadow-md grid p-4 bg-white relative'>
+            <img src="image/1.jpeg" alt='' className='w-full h-32 sm:h-48 object-cover'/>
+            <div className="flex justify-between font-semibold px-5 py-2">
+              <p className="uppercase">Sam</p>
+              <p>Ghost Beat</p>
             </div>
+              <audio controls className="">
+              <source src="audio/1.mpeg"/>
+              </audio>
             <div>
               <button className='bg-orange-600 rounded-lg text-white gap-10 p-2'>SOLD</button>
             </div>
-          </div>
-
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src='img/main.png' alt='' className='w-full h-32 sm:h-48 object-cover'/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
-            <div className='bg-orange-600 rounded-lg text-white gap-10 p-2'>
-              <button>SOLD</button>
-            </div>
-          </div>
-
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src="image/2.jpeg" alt='' className='object-cover '/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
-            <div className='bg-orange-600 rounded-lg text-white gap-10 p-2'>
-              <button>SOLD</button>
-            </div>
-          </div>
-
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src="image/3.jpeg" alt='' className='object-cover '/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
-            <div className='bg-orange-600 rounded-lg text-white gap-10 p-2'>
-              <button>SOLD</button>
-            </div>
-          </div>
-
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src='img/main.png' alt='' className='w-full h-32 sm:h-48 object-cover '/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
-            <div className='bg-orange-600 rounded-lg text-white gap-10 p-2'>
-              <button>SOLD</button>
-            </div>
-          </div>
-
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src='img/main.png' alt='' className='w-full h-32 sm:h-48 object-cover '/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
-            <div className='bg-orange-600 rounded-lg text-white gap-10 p-2'>
-              <button>SOLD</button>
-            </div>
-          </div>
-
-          <div className='rounded shadow-md p-4 object-cover'>
-            <img src='img/main.png' alt='' className='w-full h-32 sm:h-48 object-cover '/>
-            <div className='p-2'>
-              <h1 className='text-center'>CAR NAME</h1>
-            </div>
-            <div className='bg-orange-600 rounded-lg text-white gap-10 p-2'>
-              <button>SOLD</button>
+            <div className="absolute top-0 bg-white rounded-lg p-2 m-4 font-bold">
+              <span>$2000</span>
             </div>
           </div>
 
@@ -103,6 +55,7 @@ function Main() {
       <div className="text-white p-10 justify-items-center justify-center text-center">
         <h1>LATTEST COLLECTIONS</h1>
         <div className="pt-2">
+
         <div>
           <ReactPlayer
             className='react-player'
