5d9929e66a873067063c8f7b0c0264ad000a5a22
Ebuka <gideonebuka6@gmail.com>
2024-12-05 19:27:55 +0000

    6th commit

--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -4,10 +4,10 @@ import Footer from '../components/Footer';
 
 function Buy() {
   return (
-    <div className='h-screen   justify-items-center'>
+    <div className='h-screen justify-center justify-items-center'>
       <Navbar/>
         <div className='bg-black text-gray-400 text-left shadow-2xl shadow-white p-10 border-yellow-100'>
-            <div className='bg-white rounded-lg p-5 justify-center justify-items-center'>
+            <div className='bg-white rounded-lg p-2'>
                 <div  className='grid gap-2 pb-2'>
                     <div>
                         <p>Name</p>
@@ -29,11 +29,11 @@ function Buy() {
                     </div>
                 </div>
 
-                <div className='flex gap-1 py-2 overflow-hidden justify-between'>
-                    <div className=' overflow-hidden'>
+                <div className='flex gap-1 overflow-hidden justify-between h-20 w-full'>
+                    <div className=' overflow-hidden '>
                         <img src="image/visa.png" alt='' className=' object-cover'/>
                     </div>
-                    <div className=' overflow-hidden'>
+                    <div className=' overflow-hidden '>
                         <img src="image/mastercard.png" alt='' className=' object-cover'/>
                     </div>
                     <div className=' overflow-hidden'>
@@ -42,7 +42,7 @@ function Buy() {
                 </div>
 
                 <div className='justify-center justify-items-center pl-28 p-2'>
-                    <button className='bg-orange-600 p-4 justify-center justify-items-center rounded-lg'>SEND</button>
+                    <button className='bg-orange-600 p-4 w-full justify-center justify-items-center rounded-lg'>SEND</button>
                 </div>
             </div>
         </div>
